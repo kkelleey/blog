6c41c51e1969022ceb948088ba3af9f25c8ee7ea
kkelleey <karenkelley11@gmail.com>
2016-01-06 19:34:28 -0600

    adding user model and clearance

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
-FactoryGirl.define do  factory :post do
+FactoryGirl.define do  factory :user do
+    name "MyString"
+password "MyString"
+  end
+  factory :post do
     title "MyString"
 text "MyText"
   end

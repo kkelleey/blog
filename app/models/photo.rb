ee2b3f8174659c23bb8f76f6a33e54adefb4cbbb
kkelleey <karenkelley11@gmail.com>
2016-01-09 20:50:23 -0600

    formatting images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :country
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image,
+    styles: {
+    thumb: "100x100#",
+    small: "x250",
+    medium: "300x300" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

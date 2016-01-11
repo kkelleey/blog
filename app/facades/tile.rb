8338a192d3a92fb85f01e8e606a0fb9a91d0191c
kkelleey <karenkelley11@gmail.com>
2016-01-10 19:33:49 -0600

    adding metadata to tiles

--- app/facades/tile.rb
+++ app/facades/tile.rb
@@ -1,17 +1,37 @@
 class Tile
-  # returns array of image urls, plus :title_tile
   def self.generate_tiles(n)
-    tiles = [:title_tile]
-    tiles += get_country_logo_tiles
-    random_photos = Photo.random(n - tiles.count)
-    tiles += random_photos.map { |photo| photo.image.url(:medium) }
+    tiles = [title_tile] + country_logo_tiles
+    tiles += random_photo_tiles(n - tiles.count)
     tiles.shuffle
   end
 
   private
 
-  def self.get_country_logo_tiles
-    Country.all.map { |country| country.logo.url(:medium) }
+  def self.title_tile
+    {
+      src: :title_tile,
+      href: '#',
+      class: 'title-tile',
+    }
   end
 
+  def self.country_logo_tiles
+    Country.all.map do |country|
+      {
+        src: country.logo.url(:medium),
+        href: Rails.application.routes.url_helpers.posts_path(country: country.id),
+        class: 'logo',
+      }
+    end
+  end
+
+  def self.random_photo_tiles(n)
+    Photo.random(n).map do |photo|
+      {
+        src: photo.image.url(:medium),
+        href: '#',
+        class: 'photo',
+      }
+    end
+  end
 end

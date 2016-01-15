4c5f323a2bf117cc0f66681037308445f7cd5bc4
kkelleey <karenkelley11@gmail.com>
2016-01-15 06:37:29 -0600

    making medium logo image shrink to scale

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,7 +5,7 @@ class Country < ActiveRecord::Base
   has_attached_file :logo,
     styles: {
       thumb: "100x100#",
-      medium: "250x200#",
+      medium: "250x200>",
   }
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 end

e1bf25dda557959aceab34d3057e888e02cb38ca
kkelleey <karenkelley11@gmail.com>
2016-02-13 14:23:30 -0500

    only showing 'Show all photos from ___' if has photos

--- spec/features/user_visits_posts_pages_spec.rb
+++ spec/features/user_visits_posts_pages_spec.rb
@@ -2,12 +2,33 @@ require 'rails_helper'
 
 RSpec.feature "UserVisitsPostsPages", type: :feature do
   describe "when user visits posts page" do
-    it "displays the 5 most recent posts" do
-      posts = FactoryGirl.create_list :post, 6
+    it "displays the post's title" do
+      FactoryGirl.create :post, title: 'First post title'
       visit posts_path
 
-      expect(page).to have_content(posts.last.title)
-      expect(page).not_to have_content(posts.first.title)
+      expect(page).to have_content('First post title')
+    end
+  end
+
+  describe "when user visits posts page and post has no photos" do
+    it "does not display 'See all photos from CityWithoutPhotos'" do
+      city = FactoryGirl.create :city, name: 'CityWithoutPhotos'
+      FactoryGirl.create :post, city_id: city.id
+      visit posts_path
+
+      expect(page).not_to have_content('See all photos from CityWithoutPhotos')
+    end
+  end
+
+  describe "when user visits posts page and post has photos" do
+    it "displays 'See all photos from CityWithPhotos'" do
+      city = FactoryGirl.create :city, name: 'CityWithPhotos'
+      FactoryGirl.create :post, city_id: city.id
+      FactoryGirl.create :photo, city_id: city.id
+      visit posts_path
+
+      expect(page).to have_content('See all photos from CityWithPhotos')
     end
   end
 end
+

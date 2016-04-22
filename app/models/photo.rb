8cbf630371542ab474e8c0a62e8bc0914a6b5922
kkelleey <karenkelley11@gmail.com>
2016-04-22 09:01:02 -0500

    Fixing syntax errors in photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,16 +1,15 @@
 class Photo < ActiveRecord::Base
   belongs_to :city
   scope :with_location, -> { includes(:city, city: :country) }
-  scope :by_country, -> country do
+  scope :by_country, lambda do |country|
     joins(:city).where(cities: { country_id: country })
   end
-  scope :by_city, -> city { where(city: city) }
+  scope :by_city, -> (city) { where(city: city) }
 
   has_attached_file :image,
-    styles: {
-    thumb: "100x100#",
-    small: "x250",
-    medium: "250x200#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+                    styles: {
+                      thumb: '100x100#',
+                      small: 'x250',
+                      medium: '250x200#' }
+  validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\j}
 end

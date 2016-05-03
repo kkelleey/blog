54d91161df58c6ef0bd375b71545ffb1f818a493
Karen Kelley <karenkelley11@gmail.com>
2016-05-03 13:49:52 -0500

    Only call .with_location if by_city is not set
    Before this commit, PhotosController#index was always eager loading
    locations on the photos. Since locations aren't needed when the photos
    are filtered by city, we only need to eager load if :by_city isn't set
    in the params.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,9 +3,11 @@ class PhotosController < ApplicationController
   has_scope :by_city
 
   def index
-    @photos = apply_scopes(Photo).with_location.order(created_at: :desc)
+    @photos = apply_scopes(Photo).order(created_at: :desc)
     if params[:by_city]
       @city = City.find(params[:by_city])
+    else
+      @photos = @photos.with_location
     end
   end
 end

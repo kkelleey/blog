073ed619cb9a4935dc099895cc9701a5279ab228
Karen Kelley <karenkelley11@gmail.com>
2016-05-09 17:00:34 -0500

    Admin city show feature spec

---
+++ spec/features/admin_navigates_to_city_shows_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "AdminNavigatesToCityShows", type: :feature do
+  it 'shows the city latitude and longitude' do
+    admin = create :user
+    city = create :city
+    visit admin_cities_path(as: admin)
+    click_on_row_for(city)
+
+    expect(page).to have_content(city.latitude)
+    expect(page).to have_content(city.longitude)
+  end
+
+  def click_on_row_for(city)
+    find('td', text: city.name).click
+  end
+end
